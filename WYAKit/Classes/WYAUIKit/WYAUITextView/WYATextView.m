3d1fca45b506ca4730c7dcfdbea50bd0b3c2b483
李世航 <1228506851@qq.com>
2018-11-13 15:13:55 +0800

    添加demo文件夹

--- WYAKit/Classes/WYAUIKit/WYAUITextView/WYATextView.m
+++ WYAKit/Classes/WYAUIKit/WYAUITextView/WYATextView.m
@@ -8,7 +8,7 @@
 #import "WYATextView.h"
 
 @interface WYATextView ()<UITextViewDelegate>
-
+@property (nonatomic, copy)   NSString * lastPlaceHold;
 @end
 
 @implementation WYATextView
@@ -17,7 +17,7 @@
 {
     self = [super init];
     if (self) {
-        
+        self.delegate = self;
     }
     return self;
 }
@@ -26,27 +26,78 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
+        self.delegate = self;
     }
     return self;
 }
 
-//- (BOOL)textViewShouldBeginEditing:(UITextView *)textView;
-//- (BOOL)textViewShouldEndEditing:(UITextView *)textView;
-//
-//- (void)textViewDidBeginEditing:(UITextView *)textView;
-//- (void)textViewDidEndEditing:(UITextView *)textView;
-//
-//- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text;
-//- (void)textViewDidChange:(UITextView *)textView;
-//
-//- (void)textViewDidChangeSelection:(UITextView *)textView;
-//
-//- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange interaction:(UITextItemInteraction)interaction NS_AVAILABLE_IOS(10_0);
-//- (BOOL)textView:(UITextView *)textView shouldInteractWithTextAttachment:(NSTextAttachment *)textAttachment inRange:(NSRange)characterRange interaction:(UITextItemInteraction)interaction NS_AVAILABLE_IOS(10_0);
-//
-//- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange NS_DEPRECATED_IOS(7_0, 10_0, "Use textView:shouldInteractWithURL:inRange:forInteractionType: instead");
-//- (BOOL)textView:(UITextView *)textView shouldInteractWithTextAttachment:(NSTextAttachment *)textAttachment inRange:(NSRange)characterRange NS_DEPRECATED_IOS(7_0, 10_0, "Use textView:shouldInteractWithTextAttachment:inRange:forInteractionType: instead");
+-(void)setPlaceHold:(NSString *)placeHold{
+    self.text = placeHold;
+    self.textColor = random(234, 234, 234, 1);
+}
+
+#pragma mark --- UITextViewDelegate
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView{
+    
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextViewShouldBeginEditing:)]) {
+        return [self.wya_delegate wya_TextViewShouldBeginEditing:textView];
+    }
+    return YES;
+}
+
+- (BOOL)textViewShouldEndEditing:(UITextView *)textView{
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextViewShouldEndEditing:)]) {
+        return [self.wya_delegate wya_TextViewShouldEndEditing:textView];
+    }
+    return YES;
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView{
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextViewDidBeginEditing:)]) {
+        [self.wya_delegate wya_TextViewDidBeginEditing:textView];
+    }
+    
+}
+- (void)textViewDidEndEditing:(UITextView *)textView{
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextViewDidEndEditing:)]) {
+        [self.wya_delegate wya_TextViewDidEndEditing:textView];
+    }
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextView:shouldChangeTextInRange:replacementText:)]) {
+        return [self.wya_delegate wya_TextView:textView shouldChangeTextInRange:range replacementText:text];
+    }
+    return YES;
+}
+
+- (void)textViewDidChange:(UITextView *)textView{
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextViewDidChange:)]) {
+        [self.wya_delegate wya_TextViewDidChange:textView];
+    }
+}
+
+- (void)textViewDidChangeSelection:(UITextView *)textView{
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextViewDidChangeSelection:)]) {
+        [self.wya_delegate wya_TextViewDidChangeSelection:textView];
+    }
+}
+
+- (BOOL)textView:(UITextView *)textView shouldInteractWithURL:(NSURL *)URL inRange:(NSRange)characterRange interaction:(UITextItemInteraction)interaction NS_AVAILABLE_IOS(10_0){
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextView:shouldInteractWithURL:inRange:interaction:)]) {
+        return [self.wya_delegate wya_TextView:textView shouldInteractWithURL:URL inRange:characterRange interaction:interaction];
+    }
+    return YES;
+}
+
+- (BOOL)textView:(UITextView *)textView shouldInteractWithTextAttachment:(NSTextAttachment *)textAttachment inRange:(NSRange)characterRange interaction:(UITextItemInteraction)interaction NS_AVAILABLE_IOS(10_0){
+    if (self.wya_delegate && [self.wya_delegate respondsToSelector:@selector(wya_TextView:shouldInteractWithTextAttachment:inRange:interaction:)]) {
+        return [self.wya_delegate wya_TextView:textView shouldInteractWithTextAttachment:textAttachment inRange:characterRange interaction:interaction];
+    }
+    return YES;
+}
+
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
