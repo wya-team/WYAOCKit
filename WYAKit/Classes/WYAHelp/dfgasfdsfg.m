a4ad34259a65c2715896086dbadceb725f9d5ff8
李俊恒 <YxhHLi@163.com>
2018-11-13 14:11:42 +0800

    添加配置文件的常用宏定义

--- WYAKit/Classes/WYAHelp/dfgasfdsfg.m
+++ WYAKit/Classes/WYAHelp/dfgasfdsfg.m
@@ -8,7 +8,9 @@
 #import "dfgasfdsfg.h"
 
 @implementation dfgasfdsfg
+- (void)print{
 
+}
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
