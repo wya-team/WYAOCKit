f63631bd894fb030d4fdb53d2e2902fd16fb9c98
李世航 <1228506851@qq.com>
2018-11-15 10:26:42 +0800

    忽略上传pod库

--- Example/Pods/Target Support Files/JQFMDB/JQFMDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JQFMDB : NSObject
-@end
-@implementation PodsDummy_JQFMDB
-@end
--- Example/Pods/Target Support Files/WYAKit/WYAKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WYAKit : NSObject
-@end
-@implementation PodsDummy_WYAKit
-@end
