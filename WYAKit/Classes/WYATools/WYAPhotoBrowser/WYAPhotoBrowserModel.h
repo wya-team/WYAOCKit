6ab18d87c36aee70d8400d6b625d8db50f767d8e
李世航 <1228506851@qq.com>
2018-11-15 10:49:16 +0800

    添加WYAPhotoBrowserModel文件

---
+++ WYAKit/Classes/WYATools/WYAPhotoBrowser/WYAPhotoBrowserModel.h
@@ -0,0 +1,16 @@
+//
+//  WYAPhotoBrowserModel.h
+//  WYAKit
+//
+//  Created by 李世航 on 2018/11/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WYAPhotoBrowserModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
