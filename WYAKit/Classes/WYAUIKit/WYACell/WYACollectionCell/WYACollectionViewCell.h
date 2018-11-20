73a4cec65dc690f24742a341b1a603471f30f244
李世航 <1228506851@qq.com>
2018-11-20 11:59:13 +0800

    添加WYACell、WYASlidder组件

---
+++ WYAKit/Classes/WYAUIKit/WYACell/WYACollectionCell/WYACollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  WYACollectionViewCell.h
+//  WYAKit
+//
+//  Created by 李世航 on 2018/11/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WYACollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
