4ba19ca6d9b93cf490d807387df78950ac2415f1
李俊恒 <YxhHLi@163.com>
2018-11-22 10:12:11 +0800

    修改PageController

---
+++ Example/WYAKit/WYAUIKit/WYAPageViewController/Controllers/WYAThreeTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  WYAThreeTableViewController.h
+//  WYAKit_Example
+//
+//  Created by 李俊恒 on 2018/11/22.
+//  Copyright © 2018 WYATeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WYAThreeTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/WYAKit/WYAUIKit/WYAPageViewController/Controllers/WYATwoTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  WYATwoTableViewController.h
+//  WYAKit_Example
+//
+//  Created by 李俊恒 on 2018/11/22.
+//  Copyright © 2018 WYATeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WYATwoTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
