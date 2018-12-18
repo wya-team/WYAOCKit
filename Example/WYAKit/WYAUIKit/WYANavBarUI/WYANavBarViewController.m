7281b80889d0fd82e456749141f1c41f89e7270d
李俊恒 <YxhHLi@163.com>
2018-12-18 14:26:13 +0800

    提交导航展示示例

--- Example/WYAKit/WYAUIKit/WYANavBarUI/WYANavBarViewController.m
+++ Example/WYAKit/WYAUIKit/WYANavBarUI/WYANavBarViewController.m
@@ -9,25 +9,57 @@
 #import "WYANavBarViewController.h"
 
 @interface WYANavBarViewController ()
-
+@property (nonatomic, strong) UIView * tempView;
+@property (nonatomic, strong) WYANavBar * tempNavBar1;
+@property (nonatomic, strong) WYANavBar * tempNavBar2;
 @end
 
 @implementation WYANavBarViewController
-
+#pragma mark ======= Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Do any additional setup after loading the view.
+    [self.view addSubview:self.tempView];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark ======= getter
+- (WYANavBar *)tempNavBar1{
+    if(!_tempNavBar1){
+        _tempNavBar1 = ({
+            WYANavBar * object = [[WYANavBar alloc]init];
+            object.navTitle = @"WYANavBar";
+            [object wya_customGobackWithImage:[UIImage imageNamed:@"返回"]];
+            [object wya_addRightNavBarButtonWithNormalImage:@[@"icon_search",@"list"] highlightedImg:@[]];
+            object;
+       });
+    }
+    return _tempNavBar1;
 }
-*/
 
+- (WYANavBar *)tempNavBar2{
+    if(!_tempNavBar2){
+        _tempNavBar2 = ({
+            WYANavBar * object = [[WYANavBar alloc]init];
+            object.navTitle = @"WYANavBar";
+            [object wya_addLeftNavBarButtonWithNormalTitle:@[@"返回"] normalColor:@[[UIColor wya_hex:@"#108DE7"]] highlightedColor:@[[UIColor wya_hex:@"#FFFFFFF"]]];
+            [object wya_addRightNavBarButtonWithNormalTitle:@[@"搜索",@"更多"] normalColor:@[[UIColor wya_hex:@"#108DE7"],[UIColor wya_hex:@"#108DE7"]] highlightedColor:@[[UIColor wya_hex:@"#FFFFFFF"],[UIColor wya_hex:@"#FFFFFFF"]]];
+            object;
+       });
+    }
+    return _tempNavBar2;
+}
+
+- (UIView *)tempView{
+    if(!_tempView){
+        _tempView = ({
+            UIView * object = [[UIView alloc]initWithFrame:CGRectMake(0, WYATopHeight, ScreenWidth, 100)];
+            self.tempNavBar1.frame = CGRectMake(0, 0, ScreenWidth, 44);
+            self.tempNavBar2.frame = CGRectMake(0, WYATopHeight, ScreenWidth, 44);
+            [object addSubview:self.tempNavBar1];
+            [object addSubview:self.tempNavBar2];
+            object;
+       });
+    }
+    return _tempView;
+}
 @end
