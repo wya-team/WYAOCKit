aa86cdf72430cf8dd6e41f6552e3dcd1ed4253d4
李俊恒 <YxhHLi@163.com>
2018-12-21 16:34:07 +0800

    添加示例里边查看自定义键盘文档链接

--- Example/WYAKit/Class/Home/WYAUIKit/WYANumberKeyBoard/WYANumberKeyBoardController.m
+++ Example/WYAKit/Class/Home/WYAUIKit/WYANumberKeyBoard/WYANumberKeyBoardController.m
@@ -16,9 +16,16 @@
 @end
 
 @implementation WYANumberKeyBoardController
-
+- (void)wya_customrRightBarButtonItemPressed:(UIButton *)sender{
+    // 查看README文档
+    NSLog(@"查看文档");
+    WYAReadMeViewController * vc = [[WYAReadMeViewController alloc]init];
+    vc.readMeUrl = @"https://github.com/wya-team/WYAOCKit/blob/master/WYAKit/Classes/WYAUIKit/WYANumberKeyboard/README.md";
+    [self.navigationController pushViewController:vc animated:YES];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self wya_addRightNavBarButtonWithNormalImage:@[@"icon_help"] highlightedImg:@[]];
     [self.view addSubview:self.textField1];
     [self.view addSubview:self.textField2];
 
