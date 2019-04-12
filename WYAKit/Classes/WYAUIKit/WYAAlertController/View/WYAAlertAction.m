82f4dc1c7b93a76ceba4e9f3b3381a1e3e719d9d
XuefengLee <953894805@qq.com>
2019-04-12 13:33:32 +0800

    给alertAction添加实例化方法

--- WYAKit/Classes/WYAUIKit/WYAAlertController/View/WYAAlertAction.m
+++ WYAKit/Classes/WYAUIKit/WYAAlertController/View/WYAAlertAction.m
@@ -8,6 +8,34 @@
 @implementation WYAAlertAction
 
 + (instancetype)wya_actionWithTitle:(NSString *)title
+                            handler:(void (^)(void))handler{
+    WYAAlertAction * action = [[WYAAlertAction alloc] init];
+    action.title            = title;
+    action.handler          = handler;
+    action.textColor    = [UIColor blackColor];
+    action.textFont     = FONT(15);
+    return action;
+}
+
++ (instancetype)wya_actionWithTitle:(NSString *)title
+                          textColor:(UIColor *)textColor
+                            handler:(void (^)(void))handler{
+    
+    WYAAlertAction * action = [[WYAAlertAction alloc] init];
+    action.title            = title;
+    action.handler          = handler;
+    if (textColor) {
+        action.textColor    = textColor;
+    } else {
+        action.textColor    = [UIColor blackColor];
+    }
+    
+    action.textFont     = FONT(15);
+    return action;
+    
+}
+
++ (instancetype)wya_actionWithTitle:(NSString *)title
                           textColor:(UIColor *)textColor
                            textFont:(UIFont *)textFont
                             handler:(void (^)(void))handler {
@@ -26,5 +54,7 @@
     }
     return action;
 }
+
+
 
 @end
