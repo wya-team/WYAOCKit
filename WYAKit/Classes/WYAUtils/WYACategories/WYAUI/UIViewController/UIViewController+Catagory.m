a50feb7c50cd91dd0cfca06388ac4cbef74203d4
李世航 <1228506851@qq.com>
2019-04-30 11:33:42 +0800

    取消动态方法

--- WYAKit/Classes/WYAUtils/WYACategories/WYAUI/UIViewController/UIViewController+Catagory.m
+++ WYAKit/Classes/WYAUtils/WYACategories/WYAUI/UIViewController/UIViewController+Catagory.m
@@ -9,46 +9,46 @@
 
 @implementation UIViewController (Catagory)
 
-+ (void)load {
-    static dispatch_once_t onceToken;
+//+ (void)load {
+//    static dispatch_once_t onceToken;
+//
+//    dispatch_once(&onceToken, ^{
+//
+//        Class class          = [self class];
+//        SEL originalSelector = @selector(viewWillAppear:);
+//        SEL swizzledSelector = @selector(wya_viewWillAppear:);
+//
+//        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+//        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+//        method_exchangeImplementations(originalMethod, swizzledMethod);
+//
+//    });
+//}
 
-    dispatch_once(&onceToken, ^{
+//- (void)wya_viewWillAppear:(BOOL)animated {
+//    [self wya_viewWillAppear:animated];
+//    UIScrollView * scrollView = nil;
+//    for (UIView * view in self.view.subviews) {
+//        if ([view isKindOfClass:[UITableView class]] ||
+//            [view isKindOfClass:[UICollectionView class]]) {
+//            scrollView = (UIScrollView *)view;
+//            break;
+//        }
+//    }
+//    if (!self.automaticallyAdjustsScrollViewInsets) {
+//        if (@available(iOS 11.0, *)) {
+//            scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+//        }
+//    } else {
+//        if (@available(iOS 11.0, *)) {
+//            scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
+//        }
+//    }
+//}
 
-        Class class          = [self class];
-        SEL originalSelector = @selector(viewWillAppear:);
-        SEL swizzledSelector = @selector(wya_viewWillAppear:);
-
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        method_exchangeImplementations(originalMethod, swizzledMethod);
-
-    });
-}
-
-- (void)wya_viewWillAppear:(BOOL)animated {
-    [self wya_viewWillAppear:animated];
-    UIScrollView * scrollView = nil;
-    for (UIView * view in self.view.subviews) {
-        if ([view isKindOfClass:[UITableView class]] ||
-            [view isKindOfClass:[UICollectionView class]]) {
-            scrollView = (UIScrollView *)view;
-            break;
-        }
-    }
-    if (!self.automaticallyAdjustsScrollViewInsets) {
-        if (@available(iOS 11.0, *)) {
-            scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
-        }
-    } else {
-        if (@available(iOS 11.0, *)) {
-            scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
-        }
-    }
-}
-
-- (void)wya_setAutomaticallyAdjustsScrollViewInsets:(BOOL)automaticallyAdjustsScrollViewInsets {
-    [self wya_setAutomaticallyAdjustsScrollViewInsets:automaticallyAdjustsScrollViewInsets];
-}
+//- (void)wya_setAutomaticallyAdjustsScrollViewInsets:(BOOL)automaticallyAdjustsScrollViewInsets {
+//    [self wya_setAutomaticallyAdjustsScrollViewInsets:automaticallyAdjustsScrollViewInsets];
+//}
 
 //页面如果要修改statusBar请重写以下两个方法
 - (BOOL)prefersStatusBarHidden {
