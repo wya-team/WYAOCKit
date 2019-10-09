5b710b3a6dbd86a96a75e3d54257d80dfebc4d37
李世航 <1228506851@qq.com>
2019-10-09 11:08:35 +0800

    修复时间选择器重复选择时的bug

--- WYAKit/Classes/WYAUIKit/WYAPickerView/WYADatePicker.m
+++ WYAKit/Classes/WYAUIKit/WYAPickerView/WYADatePicker.m
@@ -16,6 +16,7 @@ static CGFloat titleHeight      = 44.0;
 @property (nonatomic, strong) WYAPaginationView * titleView;
 @property (nonatomic, strong) UIView * line;
 @property (nonatomic, strong) WYAPickerManager * datePickerManager;
+@property (nonatomic, strong) WYAPickerManager * datePickerOtherManager;
 @property (nonatomic, strong) UIPickerView * pickView;
 @property (nonatomic, copy) NSString * resultString;
 
@@ -1298,11 +1299,11 @@ static CGFloat titleHeight      = 44.0;
     WYADateCompare otherResult = [selectDate wya_dateCompareWithDate:self.maxDate];
     NSAssert(result != WYADateCompareLess && otherResult != WYADateCompareGreater, @"选中的时间不在最大时间和最小时间范围内");
 
-    WYAPickerManager * datePicker = [[WYAPickerManager alloc] init];
-    datePicker.selectDate         = selectDate;
-    datePicker.minimumDate = self.minDate;
-    datePicker.maximumDate = self.maxDate;
-    self.datePickerManager        = datePicker;
+
+    self.datePickerOtherManager.selectDate         = selectDate;
+    self.datePickerOtherManager.minimumDate = self.minDate;
+    self.datePickerOtherManager.maximumDate = self.maxDate;
+    self.datePickerManager        = self.datePickerOtherManager;
 }
 
 #pragma mark - Getter
@@ -1359,4 +1360,14 @@ static CGFloat titleHeight      = 44.0;
     return self.titleView.titleLabel;
 }
 
+
+- (WYAPickerManager *)datePickerOtherManager{
+    if(!_datePickerOtherManager){
+        _datePickerOtherManager = ({
+            WYAPickerManager * object = [[WYAPickerManager alloc]init];
+            object;
+       });
+    }
+    return _datePickerOtherManager;
+}
 @end
